782a8a943d4109e4eef91ac8f788c9314089f0a1
joeyame <josephameadows+git@gmail.com>
2023-01-02 09:14:30 -0700

    Finished skeleton of interpreter

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,11 @@ use anyhow::Result;
 use clap::Parser;
 use interface::*;
 use std::path::PathBuf;
+
 // Load in core modules
 mod interface;
+mod interpreting;
+mod parsing;
 mod scanning;
 
 fn main() -> Result<()> {
@@ -26,7 +29,14 @@ pub fn run(code: String) {
     println!("Scanning the following line:");
     println!("\"{}\"", code);
 
+    // Turn source code into tokens
     let tokens = crate::scanning::scan_source(code);
-    // let tokens = crate::scanning::scan_tokens(code);
     println!("Token list: {:#?}", tokens);
+
+    // Turn tokens into an abstract syntax tree
+    let tree = crate::parsing::Parser::parse_tokens(tokens);
+    println!("AST {:#?}", tree);
+
+    // Interpret the syntax tree
+    crate::interpreting::interpret(tree).expect("Interpreter failed");
 }
