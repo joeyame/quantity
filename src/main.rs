e34c2f054669b61c7364e0bed5ed755756e71d6e
joeyme <josephameadows@gmail.com>
2023-01-04 18:31:28 +0000

    Added links to axiom documentation

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,19 @@
 //! # The Quantity Programming Language
 //! A type-driven, mid-level programming language meant for scientific computation, mathematics, and simulation
 //!
-//! *All links on this page will redirect you to the official documentation for this project*
+//! > *All links on this page will redirect you to the official documentation for this project*
 //!
 //! Every decision made in the design of this language is based off the following axioms:
-//! 1. Behavior is defined
-//! 3. Boilerplate is wrong
-//! 8. Limitations are few
-//! 4. Values have meaning
-//! 2. Speed is implicit
-//! 6. Functionality is composed
-//! 7. Organization is important
-//! 9. Documentation is powerful
+//! 1. [Behavior is defined](https://joeyame.github.io/quantity/quantity/docs/axioms/index.html#behavior-is-defined)
+//! 3. [Boilerplate is wrong](https://joeyame.github.io/quantity/quantity/docs/axioms/index.html#boilerplate-is-wrong)
+//! 8. [Limitations are few](https://joeyame.github.io/quantity/quantity/docs/axioms/index.html#limitations-are-few)
+//! 4. [Values have meaning](https://joeyame.github.io/quantity/quantity/docs/axioms/index.html#)
+//! 2. [Speed is implicit](https://joeyame.github.io/quantity/quantity/docs/axioms/index.html#)
+//! 6. [Functionality is composed](https://joeyame.github.io/quantity/quantity/docs/axioms/index.html#)
+//! 7. [Organization is important](https://joeyame.github.io/quantity/quantity/docs/axioms/index.html#)
+//! 9. [Documentation is powerful](https://joeyame.github.io/quantity/quantity/docs/axioms/index.html#)
 //!
-//! ## Table of Contents
-//! The following table will link to the official documentation for this project
+// ! ## Table of Contents
 //!
 //! | Page                                                                     | Description                                                   |
 //! | ------------------------------------------------------------------------ | ------------------------------------------------------------- |
